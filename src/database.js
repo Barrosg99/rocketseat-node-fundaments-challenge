e6282b7ac06e9ebe058262789efbd79046ab5854
Gabriel Barros <barros.fontes@unesp.br>
2024-03-14 20:14:42 -0300

    add first routes

---
+++ src/database.js
@@ -0,0 +1,82 @@
+import fs from "node:fs/promises";
+
+const databasePath = new URL("../db.json", import.meta.url);
+
+export class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf-8")
+      .then((data) => (this.#database = JSON.parse(data)))
+      .catch(() => this.#persist());
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
+  select(table, search) {
+    let data = this.#database[table] ?? [];
+
+    if (search) {
+      data = data.filter((row) => {
+        return Object.entries(search).every(([key, value]) => {
+          return value && row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
+    }
+
+    return data;
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) this.#database[table].push(data);
+    else this.#database[table] = [data];
+
+    this.#persist();
+
+    return data;
+  }
+
+  update(table, id, data) {
+    const index = this.#database[table].findIndex((row) => row.id === id);
+
+    if (index === -1) {
+      return false;
+    } else {
+      Object.entries(data).forEach(([key, value]) => {
+        if (value) this.#database[table][index][key] = value;
+      });
+
+      this.#persist();
+      return true;
+    }
+  }
+
+  delete(table, id) {
+    const index = this.#database[table].findIndex((row) => row.id === id);
+
+    if (index === -1) {
+      return false;
+    } else {
+      this.#database[table].splice(index, 1);
+      this.#persist();
+      return true;
+    }
+  }
+
+  toggle(table, id) {
+    const index = this.#database[table].findIndex((row) => row.id === id);
+
+    if (index === -1) {
+      return false;
+    } else {
+      const isCompleted = !!this.#database[table][index]["completed_at"];
+      if (isCompleted)
+        this.#database[table][index]["completed_at"] = null
+      else this.#database[table][index]["completed_at"] = new Date();
+      this.#persist();
+      return true;
+    }
+  }
+}
---
+++ src/routes.js
@@ -0,0 +1,87 @@
+import { randomUUID } from "node:crypto";
+
+import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: "GET",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const filter = req.query;
+      const hasFitler = Object.entries(filter).length;
+
+      const tasks = database.select("tasks", hasFitler ? filter : null);
+
+      return res.end(JSON.stringify(tasks));
+    },
+  },
+  {
+    method: "POST",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const { title, description } = req.body;
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        create_at: new Date(),
+        update_at: new Date(),
+      };
+
+      database.insert("tasks", task);
+
+      return res.writeHead(201).end();
+    },
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { body } = req;
+
+      const hasUpdated = database.update("tasks", id, body);
+
+      if (hasUpdated) return res.writeHead(204).end();
+      else
+        return res
+          .writeHead(404)
+          .end(JSON.stringify({ error: "Task not found" }));
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      const hasDeleted = database.delete("tasks", id);
+
+      if (hasDeleted) return res.writeHead(204).end();
+      else
+        return res
+          .writeHead(404)
+          .end(JSON.stringify({ error: "Task not found" }));
+    },
+  },
+  {
+    method: "PATCH",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      const hasToggled = database.toggle("tasks", id);
+
+      if (hasToggled) return res.writeHead(204).end();
+      else
+        return res
+          .writeHead(404)
+          .end(JSON.stringify({ error: "Task not found" }));
+    },
+  },
+];
